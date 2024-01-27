263a615bba6def4dc4e20a9d8ec5bc9d5807892f
Flávio <f.augustosdn@gmail.com>
2024-01-26 21:25:20 -0300

    feat: estruturado e estilizado o header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import "./global.css"
-import "./App.module.css";
+import "./global.css";
+import style from "./App.module.css";
+import Rocket from "./assets/rocket.svg";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <div className={style.wrapper}>
+        <header>
+          <div>
+            <img src={Rocket} alt="imagem de um foguete roxo decolando" />
+          </div>
+          <div className={style.brand}>
+            <span>to</span>
+            <span>do</span>
+          </div>
+        </header>
+        <main>
+            
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
